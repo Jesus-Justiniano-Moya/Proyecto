53fb7bd0facbadf1df9d9abf777cea147861fd4a
Jesus-Justiniano-Moya <73489934+Jesus-Justiniano-Moya@users.noreply.github.com>
2022-11-12 00:04:32 -0500

    1
    1

---
+++ app/src/main/java/com/example/headbook/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.headbook;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button logBTN, RegistrarBTN;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        logBTN = findViewById(R.id.LogBTN);
+        RegistrarBTN = findViewById(R.id.RegistrarBTN);
+
+        logBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,Login.class));
+            }
+        });
+
+        RegistrarBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,Registro.class));
+            }
+        });
+    }
+}
